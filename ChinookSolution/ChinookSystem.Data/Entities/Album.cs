4b7760edcc0a762d6108f4f876afcb75ee53717e
pshchegol1 <46579720+pshchegol1@users.noreply.github.com>
2019-09-09 20:50:29 -0600

    Review Entity Creation
    Inroduced Virtual Navigational Properties

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+
+        private string _ReleaseLabel;
+        
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _ReleaseLabel = null;
+                }
+                else
+                {
+                    _ReleaseLabel = value;
+                } 
+            }
+        }
+       
+        
+        public virtual Artist Artist { get; set; }
+
+
+    }
+}
