c5b33217115b1d7e7b84b2b8868d04703c03cfd3
pshchegol1 <46579720+pshchegol1@users.noreply.github.com>
2019-09-13 20:46:21 -0600

    ODS Review
    Review ODS and populate dropdown list

--- ChinookSolution/WebApp/SamplePages/FilterSearchCRUD.aspx.cs
+++ ChinookSolution/WebApp/SamplePages/FilterSearchCRUD.aspx.cs
@@ -32,12 +32,40 @@ namespace WebApp.SamplePages
             ArtistList.DataTextField = nameof(Artist.Name);
             ArtistList.DataValueField = nameof(Artist.ArtistId);
             ArtistList.DataBind();
-            ArtistList.Items.Insert(0, "Select...");
+          //  ArtistList.Items.Insert(0, "Select...");
         }
 
         protected void ArtistList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+        protected void AlbumList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Standard lookup
+            GridViewRow agvrow = AlbumList.Rows[AlbumList.SelectedIndex];
+            //Retreive the value from a web control located witnin the GridView cell
+            string albumid = (agvrow.FindControl("AlbumId") as Label).Text;
+
+            //error handling will need to be added
+            AlbumController sysmgr = new AlbumController();
+            Album datainfo = sysmgr.Album_Get(int.Parse(albumid));
+            if (datainfo == null)
+            {
+                //clear the controls
+                //throw an exception
+            }
+            else
+            {
+                EditAlbumID.Text = datainfo.AlbumId.ToString();
+                EditTitle.Text = datainfo.Title;
+                EditAlbumArtistList.SelectedValue = datainfo.ArtistId.ToString();
+                EditReleaseYear.Text = datainfo.ReleaseYear.ToString();
+                EditReleaseLabel.Text = datainfo.ReleaseLabel == null ?
+                    "" : datainfo.ReleaseLabel;
+            }
+        }
+
+
     }
 }
